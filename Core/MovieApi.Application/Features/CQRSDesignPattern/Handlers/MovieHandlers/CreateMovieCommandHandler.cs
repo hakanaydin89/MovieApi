2e523aeda5f17b1c026e475277a374ac40963d3f
ASUS <ASUS@DESKTOP-82M8FE3>
2025-03-07 15:47:57 +0300

    End 8

---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Handlers/MovieHandlers/CreateMovieCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MovieApi.Domain.Entities;
+using MovieApi.Persistence.Context;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Handlers.MovieHandlers
+{
+    public class CreateMovieCommandHandler
+    {
+        private readonly MovieContext _context;
+
+        public CreateMovieCommandHandler(MovieContext context)
+        {
+            _context = context;
+        }
+
+        public async void Handle(Movie command)
+        {
+            _context.Movies.Add(new Movie
+            {
+                CoverImageUrl = command.CoverImageUrl,
+                CreatedYear = command.CreatedYear,
+                Decription = command.Decription,
+                Duration = command.Duration,
+                Rating = command.Rating,
+                ReleaseDate = command.ReleaseDate,
+                Status = command.Status,
+                Title = command.Title
+            });
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
