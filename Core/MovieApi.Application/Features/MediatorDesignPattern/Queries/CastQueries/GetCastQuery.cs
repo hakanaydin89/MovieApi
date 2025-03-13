4b5e005bf9905b36348d060d15105c2a89367127
ASUS <ASUS@DESKTOP-82M8FE3>
2025-03-13 16:24:27 +0300

    16 is over

---
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Queries/CastQueries/GetCastQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using MovieApi.Application.Features.MediatorDesignPattern.Results.CastResult;
+
+namespace MovieApi.Application.Features.MediatorDesignPattern.Queries.CastQueries
+{
+    public class GetCastQuery : IRequest<List<GetCastQueryResult>>
+    {
+    }
+}
