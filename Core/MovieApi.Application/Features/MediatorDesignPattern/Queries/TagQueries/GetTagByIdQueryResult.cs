9c6d1e683f274bc2afafb4c25925dd7cd5b41f2c
ASUS <ASUS@DESKTOP-82M8FE3>
2025-04-17 14:15:15 +0300

    19 is over

---
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Queries/TagQueries/GetTagByIdQueryResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace MovieApi.Application.Features.MediatorDesignPattern.Queries.TagQueries
+{
+    public class GetTagByIdQueryResult : IRequest<GetTagByIdQueryResult>
+    {
+        public int TagId { get; set; }
+
+        public GetTagByIdQueryResult(int tagId)
+        {
+            TagId = tagId;
+        }
+    }
+}
---
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Queries/TagQueries/GetTagQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using MovieApi.Application.Features.MediatorDesignPattern.Results.TagResult;
+
+namespace MovieApi.Application.Features.MediatorDesignPattern.Queries.TagQueries
+{
+    public class GetTagQuery : IRequest<List<GetTagQueryResult>>
+    {
+    }
+}
